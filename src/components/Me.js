9e092428f06cc68e0cc6b326ce0f4b3aade7faa3
Michael Nguyen <nguyen.michael.26@gmail.com>
2024-09-09 22:55:12 -0700

    update me.js

--- src/components/Me.js
+++ src/components/Me.js
@@ -7,10 +7,10 @@ export default function Me(){
           <div className="flex flex-col items-center justify-center w-full mb-20">
           <h1 className="font-mono title-font md:text-2xl text-3xl mb-4 font-medium text-white">🙂</h1>
             <p className="font-mono lg:w-[150%] items-center justify-center leading-relaxed text-base">
-            🎮: Baldur's Gate 3, Overwatch, Valorant, Fall Guys, Stardew Valley, Genshin Impact, and Fortnite
+            🎮: Baldur's Gate 3, Overwatch, Valorant, Genshin Impact, Fortnite, PvE, and RPG's
             <br></br>
             <br></br>
-            🎥: Marvel, Action/Adventure, Anime, and Sci-Fi
+            🎥: Marvel, Action/Adventure, and Sci-Fi
             <br></br>
             <br></br>
             🍛: Japanese Curry
@@ -19,7 +19,7 @@ export default function Me(){
             🎵: K-Pop, Pop, R&B, Electronic, Indie, Alt Rock, Classical, and LoFi
             <br></br>
             <br></br>
-            🌎: Norway, Korea, Japan, Scotland, Ireland, New Zealand, Indonesia, and Vietnam
+            🌎: Norway, Korea, Japan, Scotland, Ireland, New Zealand, Indonesia, Cambodia, and Vietnam
             </p>
           </div>
         </div>
